af7e6584ef2bbf6cf805e015f194d64e0a6f516a
Neel Shah <neelshah.sa@gmail.com>
2023-08-17 15:17:53 +0200

    Consolidate Sentry::Span::DataConventions with OpenTelemetry

--- sentry-ruby/spec/sentry/net/http_spec.rb
+++ sentry-ruby/spec/sentry/net/http_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Sentry::Net::HTTP do
         expect(request_span.start_timestamp).not_to eq(request_span.timestamp)
         expect(request_span.description).to eq("GET http://example.com/path")
         expect(request_span.data).to eq({
-          "status" => 200,
+          "http.response.status_code" => 200,
           "url" => "http://example.com/path",
           "http.method" => "GET",
           "http.query" => "foo=bar"
@@ -74,7 +74,7 @@ RSpec.describe Sentry::Net::HTTP do
         expect(request_span.start_timestamp).not_to eq(request_span.timestamp)
         expect(request_span.description).to eq("GET http://example.com/path")
         expect(request_span.data).to eq({
-          "status" => 200,
+          "http.response.status_code" => 200,
           "url" => "http://example.com/path",
           "http.method" => "GET",
         })
@@ -317,7 +317,7 @@ RSpec.describe Sentry::Net::HTTP do
         expect(request_span.start_timestamp).not_to eq(request_span.timestamp)
         expect(request_span.description).to eq("GET http://example.com/path")
         expect(request_span.data).to eq({
-          "status" => 200,
+          "http.response.status_code" => 200,
           "url" => "http://example.com/path",
           "http.method" => "GET",
         })
@@ -329,7 +329,7 @@ RSpec.describe Sentry::Net::HTTP do
         expect(request_span.start_timestamp).not_to eq(request_span.timestamp)
         expect(request_span.description).to eq("GET http://example.com/path")
         expect(request_span.data).to eq({
-          "status" => 404,
+          "http.response.status_code" => 404,
           "url" => "http://example.com/path",
           "http.method" => "GET",
         })
